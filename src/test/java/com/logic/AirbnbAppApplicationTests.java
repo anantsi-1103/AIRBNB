520d6281a0203c77adf7f4395574c4d5c53d6cec
Anant <acera52024@gmail.com>
2026-04-12 12:35:55 +0530

    airbnb

---
+++ src/test/java/com/logic/AirbnbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
